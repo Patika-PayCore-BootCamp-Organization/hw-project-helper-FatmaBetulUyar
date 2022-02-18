176ca153d8e4e916ee62891b29fca9d6dfe241b9
Betül <69481739+FatmaBetulUyar@users.noreply.github.com>
2022-02-18 18:14:38 +0300

    Helper project created.

---
+++ src/main/java/com/patika/paycore/HelperProject/HelperProject/HelperProjectApplication.java
@@ -0,0 +1,13 @@
+package com.patika.paycore.HelperProject.HelperProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelperProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patika/paycore/HelperProject/HelperProject/HelperProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.paycore.HelperProject.HelperProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
